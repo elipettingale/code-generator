666344bad64f33056aee7a19aa3ff131d5d59fa0
Elliot Pettingale <elipettingale@gmail.com>
2018-12-12 13:40:24 +0000

    refactor profile methods

--- src/CodeGenerator.php
+++ src/CodeGenerator.php
@@ -6,27 +6,46 @@ class CodeGenerator
 {
     private $profile;
 
-    private function profiles(): array
+    private function map(string $path = null)
+    {
+        $map = json_decode(
+            file_get_contents(__DIR__ . '/../../../../../.code_generation/map.json'), true
+        );
+
+        if ($path !== null) {
+            if (!array_key_exists($path, $map)) {
+                return null;
+            }
+
+            return $map[$path];
+        }
+
+        return $map;
+    }
+
+    private function profile(string $name): array
     {
         return json_decode(
-            file_get_contents(__DIR__ . '/../../../../../.code_generation/profiles.json'), true
+            file_get_contents(__DIR__ . "/../../../../../.code_generation/profiles/{$name}.json"), true
         );
     }
 
     private function hasProfile(string $path): bool
     {
-        return array_key_exists($path, $this->profiles());
+        return $this->map($path) !== null;
     }
 
-    private function setProfile(string $path): void
+    private function loadProfile(string $path): void
     {
-        $this->profile = $this->profiles()[$path];
+        $this->profile = $this->profile(
+            $this->map($path)
+        );
     }
 
     public function load(string $path): bool
     {
         if ($this->hasProfile($path)) {
-            $this->setProfile($path);
+            $this->loadProfile($path);
 
             return true;
         }
