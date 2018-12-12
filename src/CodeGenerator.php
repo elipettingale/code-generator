df0a8b278e98c29c536dada4f2745a9958337858
Elliot Pettingale <elipettingale@gmail.com>
2018-12-12 12:26:51 +0000

    create methods to load profile

--- src/CodeGenerator.php
+++ src/CodeGenerator.php
@@ -4,9 +4,32 @@ namespace EliPett\CodeGeneration;
 
 class CodeGenerator
 {
+    private $profile;
+
+    private function profiles(): array
+    {
+        return json_decode(
+            file_get_contents(__DIR__ . '/../../../../../.code_generation/profiles.json'), true
+        );
+    }
+
+    private function hasProfile(string $path): bool
+    {
+        return array_key_exists($path, $this->profiles());
+    }
+
+    private function setProfile(string $path): void
+    {
+        $this->profile = $this->profiles()[$path];
+    }
+
     public function load(string $path): bool
     {
-        // todo: load profile
+        if ($this->hasProfile($path)) {
+            $this->setProfile($path);
+
+            return true;
+        }
 
         return false;
     }
